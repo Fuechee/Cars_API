349f87cadf19c7c7cbf032420b84c179656f4b76
Fuechee <f9vang@gmail.com>
2022-09-09 12:08:10 -0500

    Changed Database Settings + Created Cars App

---
+++ cars_project/local_setting.py
@@ -0,0 +1,15 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-0njxs7)f-%6!h8dyrpm=e7_x6o%r_%-4vk!*si0vzk9@mylief'
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cars_database',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'BeL7y$45peeK'
+    }
+}
